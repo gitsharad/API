8ae24fc146c4a54ef2b8f297bcc212e3a1c69fc7
sharad <sharad.pawar@pb.com>
2018-10-25 06:05:17 +0530

    added userType fieldin sign up and email functionality.

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const schema = mongoose.Schema
 
 const UserSchema = new schema ({
     email: String ,
-    password: String 
+    password: String,
+    userType: String 
 })
 
 module.exports = mongoose.model('user', UserSchema,'users')
--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = require('../models/user')
-const db = 'mongodb://sharad:sharadpawar1989@ds143262.mlab.com:43262/rentdb'
+// const db = 'mongodb://sharad:sharadpawar1989@ds143262.mlab.com:43262/rentdb'
+const db = 'mongodb://sharad:sharadpawar1989@ds141043.mlab.com:41043/contentwriters_db'
 const jwt = require('jsonwebtoken')
+const nodemailer = require('nodemailer');
 mongoose.connect(db, err => {
     if(err){
         console.error('Error!'+ err)
@@ -11,6 +13,22 @@ mongoose.connect(db, err => {
         console.log('connected to mongoDB')
     }
 })
+/* var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'ssspawar25@gmail.com',
+      pass: 'SharadPawar@1989'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'ssspawar25@gmail.com',
+    to: 'ssspawar25@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  }; */
+  
+  
 router.get('/', (req, res) => {
     res.send('From API Route')
 })
@@ -18,13 +36,22 @@ router.get('/', (req, res) => {
 router.post('/register',(req,res)=>{
    let userData = req.body
    let user = new User(userData)
+  
    user.save((error,registeredUser) => {
        if(error){
            console.error(error)
        }else {
            let payload = {subject: user._id}
            let token = jwt.sign(payload, 'secretkey')
-           res.status(201).send({token})
+         /*  transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+                res.status(201).send({token})
+            }
+          }); */
+          res.status(201).send({token})
+           
        }
    })
 })
@@ -65,5 +92,6 @@ router.post('/login',(req,res)=>{
      req.userId = payLoad.subject;
      next()
  }
+ 
 
 module.exports = router
